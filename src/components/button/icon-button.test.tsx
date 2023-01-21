622d9b463a0b72adfe0c9f8d982e88b8ca62620d
Bayathy <misosoup0321@icloud.com>
2023-01-21 21:25:47 +0900

    feat: write icon-button story

--- src/components/button/icon-button.test.tsx
+++ src/components/button/icon-button.test.tsx
@@ -1,1 +1,15 @@
-import { render } from "@testing-library/react";
+import { composeStories } from "@storybook/testing-react";
+import * as ButtonStories from "./styled-button.stories";
+import { render, screen, within } from "@testing-library/react";
+import React from "react";
+import { IconButton } from "./button";
+
+const { Default } = composeStories(ButtonStories);
+
+describe("testing button component", () => {
+  it("take snapshot", () => {
+    const { asFragment } = render(<Default>Button</Default>);
+    expect(asFragment()).toMatchSnapshot();
+    screen.debug();
+  });
+});
